b04ac0d031c37b033a3de6933a3a345561dda6ca
akashvdubey <akashvdubey@users.noreply.github.com>
2019-03-22 16:41:31 +0530

    index and notes routes created

--- application.py
+++ application.py
@@ -1,10 +1,9 @@
 import os
 
-from flask import Flask, session,render_template,request
+from flask import Flask,session,render_template,request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
-
 app = Flask(__name__)
 
 # Check for environment variable
@@ -15,6 +14,7 @@ app = Flask(__name__)
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
+storedNotes=[]
 
 # Set up database
 engine = create_engine("postgres://gfbbhklmbigcpt:aef99b41334f0d7e5d1e89bc87d8be92a34dd679d67d148cb6eef48716742818@ec2-54-225-95-183.compute-1.amazonaws.com:5432/d5fttg3bfrqigh")
@@ -26,13 +26,18 @@ def index():
     books=db.execute("SELECT * from books_details LIMIT 20").fetchall()
     return render_template("index.html",books=books)
 
-@app.route("/search",methods=["GET","POST"])
+@app.route("/search",methods=["GET"])
 def search():
     keyword=request.form.get("keyword")
     result=db.execute("SELECT * FROM books_details where title LIKE '%{}%'".format(keyword)).fetchall()
     return render_template("search.html",keyword=keyword,books=result,count=len(result))
 
-
+@app.route("/notes",methods=["GET"])
+def notes():
+    if request.method=="GET":
+        note=request.form.get("note")
+        storedNotes.append(note)
+    return render_template("notes.html",notes=storedNotes)
 
 
 app.run(debug=True)
