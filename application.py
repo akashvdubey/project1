6db66196fa48b0b65751017dd0b3ee5cbce0288f
akashvdubey <akashvdubey@users.noreply.github.com>
2019-12-28 08:46:01 -0200

    changes

--- application.py
+++ application.py
@@ -14,16 +14,15 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-storedNotes=[]
 
 # Set up database
-engine = create_engine("postgres://gfbbhklmbigcpt:aef99b41334f0d7e5d1e89bc87d8be92a34dd679d67d148cb6eef48716742818@ec2-54-225-95-183.compute-1.amazonaws.com:5432/d5fttg3bfrqigh")
+engine = create_engine("postgres://xsweikphirgtir:aa419190d68c241e7ec1071974e9d91bfd6363b0d0469a51267b475eee6da70c@ec2-23-21-129-125.compute-1.amazonaws.com:5432/ddbn8ef12eoni1")
 db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/", methods=["GET","POST"])
 def index():
     if 'username' in session:
-        return render_template("index.html",heading=f"welcome mr {session['username']} to book rating website")
+        return render_template("index.html",heading=f"currently Logged in as  {session['username']}")
     data=jsonify({"success":"false","error":{"code":101,"type":"missing_access_key","info":"You have not supplied an API Access Key. [Required format: access_key=YOUR_ACCESS_KEY]"}})
     print(data)
     #books=db.execute("SELECT * from books_details LIMIT 20").fetchall()
@@ -35,7 +34,6 @@ def index():
 def signup():
     username = request.form.get("username")
     password = request.form.get("password")
-    #jsonify({"username":username,"password":password})
     if len(username)<8 or len(password)<8:
         return render_template("index.html",heading="length of username or password is less than 8")
     try:
@@ -53,6 +51,7 @@ def login():
         details_of_user=db.execute("SELECT * FROM users_information WHERE username=:username and password=:password",{"username":request.form['username'],"password":request.form['password']}).fetchall()
         if(details_of_user):
             session['username'] = request.form['username']
+            session['id']= details_of_user[0][0]
             return redirect(url_for('index'))
         else:
             return render_template("index.html",heading="incorrect username or password")
@@ -65,18 +64,22 @@ def logout():
 @app.route("/search",methods=["GET","POST"])
 def search():
     keyword=request.form.get("keyword")
-    if(keyword is not None):
+    if(keyword != "" and keyword is not None):
         result=db.execute("SELECT * FROM books_details where title LIKE '%{}%'".format(keyword)).fetchall()
         return render_template("search.html",keyword=keyword,books=result,count=len(result))
     else:
-        return render_template("search.html",keyword=keyword)
+        return render_template("search.html",keyword="")
 
-@app.route("/notes",methods=["GET"])
-def notes():
-    if request.method=="GET":
-        note=request.form.get("note")
-        storedNotes.append(note)
-    return render_template("notes.html",notes=storedNotes)
-
-
+@app.route("/review/<isbn>/",methods=['GET','POST'])
+def review(isbn):
+    print("your isbn is ",isbn)
+    if(request.form.get("review") is None):
+        book=db.execute("SELECT * from books_details where isbn=:isbn",{"isbn":isbn}).fetchone()
+        return render_template("review.html",book_title=book,isbn=isbn)
+    else:
+        review= request.form.get("review")
+        points=request.form.get("points")
+        username=session["username"]
+        db.execute("INSERT INTO book_reviews values({},:rev,:isbn,{})".format(points,session['id']),{"rev":review,"isbn":isbn})
+        return "success"
 app.run(debug=True)
